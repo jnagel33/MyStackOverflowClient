7bb96ba362b1ff985cd74ab5ed2e0adadad6c002
Josh Nagel <jnagel33@gmail.com>
2015-05-13 18:04:04 -0700

    commit

--- MyStackOverflow/SearchQuestionsViewController.m
+++ MyStackOverflow/SearchQuestionsViewController.m
@@ -10,6 +10,11 @@
 #import "StackOverFlowStyleKit.h"
 #import "StackOverflowService.h"
 #import "QuestionsTableViewCell.h"
+#import "ImageService.h"
+#import "Question.h"
+#import "ImageResizer.h"
+
+const CGFloat kProfileImageSizeWidthHeight = 50;
 
 @interface SearchQuestionsViewController () <UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
@@ -30,19 +35,41 @@
   self.tableView.delegate = self;
   self.searchBar.delegate = self;
   
+  self.tableView.estimatedRowHeight = 90;
+  self.tableView.rowHeight = UITableViewAutomaticDimension;
+  
  }
 
 #pragma mark - UITableViewDataSource
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return self.questions.count;
+  return 1;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   QuestionsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"QuestionCell" forIndexPath:indexPath];
-  Question *question = self.questions[indexPath.row];
+  Question *question = self.questions[indexPath.section];
   [cell configureCell:question];
   return cell;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return self.questions.count;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+  return @" ";
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+  return 12;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+  UIView *headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 12)];
+  headerView.backgroundColor = [StackOverFlowStyleKit orange];
+  
+  return headerView;
 }
 
 #pragma mark - UISearchBarDelegate
@@ -56,6 +83,20 @@
       } else {
         self.questions = items;
         [self.tableView reloadData];
+        
+        NSMutableArray *urls = [[NSMutableArray alloc]init];
+        for (Question *question in self.questions) {
+          [urls addObject:question.profileImageURL];
+        }
+        ImageService *myImageService = [[ImageService alloc]init];
+          [myImageService fetchProfileImages:urls completionHandler:^(NSArray *images) {
+            for (int i=0; i < self.questions.count; i++) {
+              Question *question = self.questions[i];
+              question.profileImage = [ImageResizer resizeImage:images[i] withSize:CGSizeMake(kProfileImageSizeWidthHeight, kProfileImageSizeWidthHeight)];
+            }
+            [self.tableView reloadData];
+          }];
+        
       }
     }];
   }
