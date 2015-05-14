d989082266366276f9671c1399447ab09812ea51
Josh Nagel <jnagel33@gmail.com>
2015-05-13 18:03:51 -0700

    commit

---
+++ MyStackOverflow/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  MyStackOverflow
+//
+//  Created by Josh Nagel on 5/13/15.
+//  Copyright (c) 2015 jnagel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
--- MyStackOverflow/QuestionsTableViewCell.m
+++ MyStackOverflow/QuestionsTableViewCell.m
@@ -8,6 +8,8 @@
 
 #import "QuestionsTableViewCell.h"
 #import "Question.h"
+#import "ImageResizer.h"
+#import "StackOverFlowStyleKit.h"
 
 @interface QuestionsTableViewCell ()
 
@@ -16,6 +18,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *createdAtLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *checkmarkImageView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 
 @end
 
@@ -28,11 +31,24 @@
   self.profileImageView.image = nil;
   self.checkmarkImageView.image = nil;
   
+  self.containerView.layer.cornerRadius = 10;
+  
   self.titleLabel.text = question.title;
   self.tagsLabel.text = [question.tags componentsJoinedByString:@", "];
   self.createdAtLabel.text = question.createdAt;
   if (question.isAnswered) {
     self.checkmarkImageView.image = [UIImage imageNamed:@"CheckmarkStackOverflow"];
+  }
+  self.profileImageView.transform = CGAffineTransformMakeScale(0.7, 0.7);
+  if (question.profileImage) {
+    self.profileImageView.image = question.profileImage;
+    self.profileImageView.layer.borderColor = [StackOverFlowStyleKit lightGray].CGColor;
+    self.profileImageView.layer.borderWidth = 2.0;
+    self.profileImageView.alpha = 0;
+    [UIView animateWithDuration:0.3 animations:^{
+      self.profileImageView.transform = CGAffineTransformMakeScale(1, 1);
+      self.profileImageView.alpha = 1;
+    }];
   }
 }
 
